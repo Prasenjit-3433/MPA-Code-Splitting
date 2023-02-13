c31525bc5cae71833e1f9750ad5b0e2ed7169500
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-02-13 13:55:41 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import HelloWorldButton from "./components/hello-world-button/hello-world-button";
+import Heading from "./components/heading/heading";
+
+const heading = new Heading();
+heading.render();
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('Production Mode!');
+} else if (process.env.NODE_ENV === 'development') {
+    console.log('Development Mode!');
+}
+
+helloWorldButton.methodThatDoesNotExist();
